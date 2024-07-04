c67dd5ff9a60bff94eaad5f0f062c72266001439
NikiPshg <nicitacom2017@gmail.com>
2024-07-04 12:09:40 +0300

    add area fix bugs
    add area fix bugs

--- hh_mtusi/hhapp/models.py
+++ hh_mtusi/hhapp/models.py
@@ -5,6 +5,7 @@ class Vacancy(models.Model):
     name = models.CharField(max_length=255)  # Название вакансии
     company = models.CharField(max_length=255)  # Название компании
     salary = models.DecimalField(max_digits=10, decimal_places=2, default=1.00 ,null=True)
+    area = models.DecimalField(max_digits=10, decimal_places=2, default=1.00 ,null=True)
     url = models.URLField()  # URL вакансии
     published_at = models.DateTimeField()  # Дата публикации
 
--- hh_mtusi/hhapp/utils.py
+++ hh_mtusi/hhapp/utils.py
@@ -1,5 +1,6 @@
 import requests
 from .models import Vacancy
+import random
 from fake_useragent import UserAgent
 
 
@@ -10,18 +11,21 @@ def chek_salary(item):
     except:
         return 1.0
 
-def fetch_and_save_vacancies(keyword, page=0, per_page=20 , salary=None):
+def get_response(keyword, per_page=20 , area=1):
     ua = UserAgent()
     url = 'https://api.hh.ru/vacancies/'
     params = {
         'text': keyword,
-        'page': page,
+        'page': random.randint(0,10),
         'per_page': per_page,
-        'area': 1
+        'area': area
     }
     random_user_agent = ua.random
     headers = {'User-Agent': random_user_agent}
     response = requests.get(f"{url}", params=params, headers=headers)
+    return response , area
+
+def receiving_data_from_response(response):
     data = response.json()
     for item in data['items']:
         hh_id = item['id']
@@ -30,9 +34,10 @@ def fetch_and_save_vacancies(keyword, page=0, per_page=20 , salary=None):
         salary = chek_salary(item)
         url = item['alternate_url']
         published_at = item['published_at']
-
-        # Сохраняем вакансию в базе данных
-        Vacancy.objects.update_or_create(
+    return hh_id , name , company , salary , url , published_at
+ 
+def save_to_db(hh_id:int , name:str , company:str , salary:int , url , published_at , area):
+    Vacancy.objects.update_or_create(
             hh_id=hh_id,
             defaults={
                 'name': name,
@@ -40,5 +45,13 @@ def fetch_and_save_vacancies(keyword, page=0, per_page=20 , salary=None):
                 'salary' :salary,
                 'url': url,
                 'published_at': published_at,
+                'area' : area
             }
         )
+
+def fetch_and_save_vacancies(keyword):
+    response , area = get_response(keyword,area=1)
+    hh_id , name , company , salary , url , published_at  = receiving_data_from_response(response)
+    save_to_db(hh_id , name , company , salary , url , published_at , area)
+    
+
