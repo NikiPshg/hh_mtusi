485ad24be47c324a0b33293968b0b96031851aca
NikiPshg <nicitacom2017@gmail.com>
2024-07-04 12:24:39 +0300

    fix
    чут чут читабельнее

--- hh_mtusi/hhapp/utils.py
+++ hh_mtusi/hhapp/utils.py
@@ -23,19 +23,8 @@ def get_response(keyword, per_page=20 , area=1):
     random_user_agent = ua.random
     headers = {'User-Agent': random_user_agent}
     response = requests.get(f"{url}", params=params, headers=headers)
-    return response , area
+    return response 
 
-def receiving_data_from_response(response):
-    data = response.json()
-    for item in data['items']:
-        hh_id = item['id']
-        name = item['name']
-        company = item['employer']['name']
-        salary = chek_salary(item)
-        url = item['alternate_url']
-        published_at = item['published_at']
-    return hh_id , name , company , salary , url , published_at
- 
 def save_to_db(hh_id:int , name:str , company:str , salary:int , url , published_at , area):
     Vacancy.objects.update_or_create(
             hh_id=hh_id,
@@ -49,9 +38,22 @@ def save_to_db(hh_id:int , name:str , company:str , salary:int , url , published
             }
         )
 
+
+def receiving_data_from_response(response):
+    data = response.json()
+    for item in data['items']:
+        hh_id = item['id']
+        name = item['name']
+        company = item['employer']['name']
+        salary = chek_salary(item)
+        url = item['alternate_url']
+        published_at = item['published_at']
+        area = item['area']['id']
+        save_to_db(hh_id , name , company , salary , url , published_at , area)
+ 
+
 def fetch_and_save_vacancies(keyword):
-    response , area = get_response(keyword,area=1)
-    hh_id , name , company , salary , url , published_at  = receiving_data_from_response(response)
-    save_to_db(hh_id , name , company , salary , url , published_at , area)
+    response = get_response(keyword,area=1)
+    receiving_data_from_response(response)
     
 
