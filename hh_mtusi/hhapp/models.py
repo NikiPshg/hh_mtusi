d7417f3e48bd4c4850d55fc502a7e527f7f0f817
NikiPshg <nicitacom2017@gmail.com>
2024-07-04 10:37:53 +0300

    html-fix
    fix fix

--- hh_mtusi/hhapp/models.py
+++ hh_mtusi/hhapp/models.py
@@ -4,6 +4,7 @@ class Vacancy(models.Model):
     hh_id = models.CharField(max_length=100, unique=True)  # ID вакансии на hh.ru
     name = models.CharField(max_length=255)  # Название вакансии
     company = models.CharField(max_length=255)  # Название компании
+    salary = models.DecimalField(max_digits=10, decimal_places=2, default=1.00 ,null=True)
     url = models.URLField()  # URL вакансии
     published_at = models.DateTimeField()  # Дата публикации
 
--- hh_mtusi/hhapp/utils.py
+++ hh_mtusi/hhapp/utils.py
@@ -3,23 +3,31 @@ from .models import Vacancy
 from fake_useragent import UserAgent
 
 
-def fetch_and_save_vacancies(keyword, page=0, per_page=20):
+
+def chek_salary(item):
+    try:
+        return item['salary']['from']
+    except:
+        return 1.0
+
+def fetch_and_save_vacancies(keyword, page=0, per_page=20 , salary=None):
     ua = UserAgent()
-    url = 'https://api.hh.ru/vacancies'
+    url = 'https://api.hh.ru/vacancies/'
     params = {
         'text': keyword,
         'page': page,
-        'per_page': per_page
+        'per_page': per_page,
+        'area': 1
     }
     random_user_agent = ua.random
     headers = {'User-Agent': random_user_agent}
-    response = requests.get(url, params=params, headers=headers)
+    response = requests.get(f"{url}", params=params, headers=headers)
     data = response.json()
-
     for item in data['items']:
         hh_id = item['id']
         name = item['name']
         company = item['employer']['name']
+        salary = chek_salary(item)
         url = item['alternate_url']
         published_at = item['published_at']
 
@@ -29,6 +37,7 @@ def fetch_and_save_vacancies(keyword, page=0, per_page=20):
             defaults={
                 'name': name,
                 'company': company,
+                'salary' :salary,
                 'url': url,
                 'published_at': published_at,
             }
